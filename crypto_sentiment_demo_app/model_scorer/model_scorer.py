934584485fe2cc9feb2cf1fe827ced5185062665
Yury Kashnitsky <foofest@gmail.com>
2022-05-23 13:29:44 +0200

    change error handling for mdoel scorer

--- crypto_sentiment_demo_app/model_scorer/model_scorer.py
+++ crypto_sentiment_demo_app/model_scorer/model_scorer.py
@@ -101,13 +101,15 @@ class ModelScorer:
                     self.write_preds_to_db(pred_df)
                     print(f"Wrote predictions for {len(df)} records into model_predictions.")  # TODO: set up logging
 
+            # TODO: fix duplicates better
+            except IntegrityError as e:
+                print(e)
+                pass
+
+            finally:
                 # There're max ~180 news per day, and the parser get's 50 at a time,
                 # so it's fine to sleep for a quarter of a day a day
                 sleep(21600)
-
-            # TODO: fix duplicates better
-            except IntegrityError:
-                pass
 
 
 def main():
