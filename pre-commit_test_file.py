8704a48aa179fa43a389b89257bfa8633be408b4
aleksandrinvictor <victor@42930.example.com>
2022-05-29 14:16:24 +0200

    fix: configs

--- pre-commit_test_file.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def entropy(probs: np.ndarray) -> float:
-    """very long docstring violating flake8 max line ------------------------------------------------------------------"""
-    return (-probs * np.log2(probs)).sum()
