68d6c4de002f9333143e00ca6604ddf3ff8716b0
aleksandrinvictor <victor@42930.example.com>
2022-06-05 15:01:26 +0200

    fix: tests run

---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker compose -f docker-compose.test.yml build --no-cache
+
+docker run -v $PWD:/root image_model_fast_api_test pytest tests/model_inference_api; output_model_inference=$?
+docker run -v $PWD:/root image_model_scorer_test pytest tests/model_scorer; output_model_scorer=$?
+
+! (( $output_model_inference || $output_model_scorer ))
