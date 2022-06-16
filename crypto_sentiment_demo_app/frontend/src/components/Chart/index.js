f1c2cd6c0e8f2ea60fbdcd1bfe2df1d9cad7fd4b
Yury Kashnitsky <foofest@gmail.com>
2022-06-16 10:32:47 +0200

    fix data key for the X axis of the graph

--- crypto_sentiment_demo_app/frontend/src/components/Chart/index.js
+++ crypto_sentiment_demo_app/frontend/src/components/Chart/index.js
@@ -67,7 +67,7 @@ function Chart({ chartData }) {
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
+      <XAxis dataKey="pub_date" />
       <YAxis />
       <Tooltip />
       <Line
